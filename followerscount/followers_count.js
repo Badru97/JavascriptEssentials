005f7c536cf9d56d441f1b553e4afc1fd0bf7093
Badru97 <danielduskuw@gmail.com>
2024-02-21 13:20:34 -0500

    reset button added

--- followerscount/followers_count.js
+++ followerscount/followers_count.js
@@ -15,3 +15,7 @@ function checkCountValue() {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
 }
+function resetCount(){
+    document.getElementById("countDisplay").innerHTML=0;
+    alert("The followers count has been reset")
+}
